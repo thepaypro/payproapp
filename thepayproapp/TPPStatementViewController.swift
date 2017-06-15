06c1c80ba75428cd57fc5927c65d7a93ed0153d9
Enric Giribet <kike@Mac-mini-de-Enric.local>
2017-06-15 17:28:41 +0200

    Change visual details

--- thepayproapp/TPPStatementViewController.swift
+++ thepayproapp/TPPStatementViewController.swift
@@ -29,24 +29,16 @@ class TPPStatementViewController: UIViewController {
         }, type: "end")
     }
     
-    @IBOutlet weak var datePickerEnds: UIDatePicker! {
-        didSet {
-            datePickerEnds.isHidden = true
-            datePickerEnds.frame.size.height = 0
-            datePickerEnds.alpha = 0
-        }
-    }
+    @IBOutlet weak var datePickerEnds: UIDatePicker!
     @IBOutlet weak var labelDateStart: UILabel!
     @IBOutlet weak var labelDateEnd: UILabel!
     @IBOutlet weak var viewEmailSend: UIView!
-    @IBOutlet weak var lineViewBottomEnds: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         datePickerStarts.addTarget(self, action: #selector(dateStartChanged(_:)), for: .valueChanged)
         datePickerEnds.addTarget(self, action: #selector(dateEndChanged(_:)), for: .valueChanged)
-        self.viewPickerStarts.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,6 +47,14 @@ class TPPStatementViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        viewPickerStarts.frame.size.height = 0
+        datePickerStarts.frame.size.height = 0
+        datePickerStarts.alpha = 0
+        
+        viewPickerEnds.frame.size.height = 0
+        datePickerEnds.frame.size.height = 0
+        datePickerEnds.alpha = 0
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd MMMM yyyy"
         labelDateStart.text = dateFormatter.string(from: Date())
@@ -76,21 +76,17 @@ class TPPStatementViewController: UIViewController {
     func animate(duration:Double, c: @escaping () -> Void, type:String) {
         UIView.animateKeyframes(withDuration: duration, delay: 0, options: .calculationModePaced, animations: {
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: duration, animations: {
-                
+                print(self.viewPickerStarts.frame.size.height)
                 if type == "start" {
-                    if self.viewPickerStarts.isHidden == false {
-                        self.viewPickerStarts.alpha = 0
+                    if self.datePickerStarts.alpha == 1 {
                         self.viewPickerStarts.frame.size.height = 0
                         self.datePickerStarts.frame.size.height = 0
                         self.datePickerStarts.alpha = 0
                         
                         self.viewEnds.frame.origin.y -= 150
                         self.viewPickerEnds.frame.origin.y -= 150
-                        
-                        self.viewPickerStarts.isHidden = true
+                        self.viewEmailSend.frame.origin.y -= 150
                     } else {
-                        self.viewPickerStarts.isHidden = false
-                        self.viewPickerStarts.alpha = 1
                         self.viewPickerStarts.frame.size.height += 150
                         
                         self.datePickerStarts.isHidden = false
@@ -99,24 +95,21 @@ class TPPStatementViewController: UIViewController {
                         
                         self.viewEnds.frame.origin.y += 150
                         self.viewPickerEnds.frame.origin.y += 150
+                        self.viewEmailSend.frame.origin.y += 150
                     }
                 }
                 
                 if type == "end" {
-                    if self.datePickerEnds.isHidden == false {
-                        self.datePickerEnds.isHidden = true
-                        self.lineViewBottomEnds.frame.origin.y -= 150
-                        
+                    if self.datePickerEnds.alpha == 1 {
                         self.viewEmailSend.frame.origin.y -= 150
                         
+                        self.viewPickerEnds.frame.size.height -= 150
                         self.datePickerEnds.frame.size.height -= 150
                         self.datePickerEnds.alpha = 0
                     } else {
-                        self.datePickerEnds.isHidden = false
-                        self.lineViewBottomEnds.frame.origin.y += 150
-                        
                         self.viewEmailSend.frame.origin.y += 150
                         
+                        self.viewPickerEnds.frame.size.height += 150
                         self.datePickerEnds.frame.size.height += 150
                         self.datePickerEnds.alpha = 1
                     }
