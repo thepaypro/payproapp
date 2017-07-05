987ba7fd0bbe1e59ebada0b8f4d9f14d82357040
Manuel Ortega Cordovilla <manuel@mondeapp.com>
2017-07-05 13:22:46 +0200

    Frontend for account selection

--- thepayproapp/Colors.swift
+++ thepayproapp/Colors.swift
@@ -11,10 +11,13 @@ import UIKit
 struct PayProColors {
     static let alert = UIColor(red: 254.0 / 255.0, green: 56.0 / 255.0, blue: 36.0 / 255.0, alpha: 1.0)
     static let black = UIColor(red: 3.0 / 255.0, green: 3.0 / 255.0, blue: 3.0 / 255.0, alpha: 1.0)
+    static let lightBlue = UIColor (red: 1.0, green: 224.0 / 255.0, blue: 1.0, alpha: 1.0)
     static let blue = UIColor(red: 102.0 / 255.0, green: 102.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
     static let darkBlue = UIColor(red: 61.0 / 255.0, green: 61.0 / 255.0, blue: 122.0 / 255.0, alpha: 1.0)
     static let grey = UIColor(red: 143.0 / 255.0, green: 142.0 / 255.0, blue: 148.0 / 255.0, alpha: 1.0)
     static let grey2 = UIColor(red: 164.0 / 255.0, green: 170.0 / 255.0, blue: 179.0 / 255.0, alpha: 1.0)
     static let line = UIColor(red: 200.00 / 255.0, green: 199.00 / 255.00, blue: 204.00 / 255.00, alpha: 1.0)
     static let background = UIColor(red: 239.00 / 255.0, green: 239.00 / 255.00, blue: 244.00 / 255.00, alpha: 1.0)
+    static let green = UIColor(red: 29.0 / 255.0, green: 186.0 / 255.0, blue: 197.0 / 255.0, alpha: 1.0)
+    static let lightGreen = UIColor(red: 128.0 / 255.0, green: 242.0 / 255.0, blue: 170.0 / 255.0, alpha: 1.0)
 }
