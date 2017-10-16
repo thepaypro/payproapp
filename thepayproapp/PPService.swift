c89d2c0ea68172c3a9b4822be32f6a74ed9992fb
ottegu <>
2017-10-16 12:51:39 +0200

    Compatibility for iOS 11

--- thepayproapp/PPService.swift
+++ thepayproapp/PPService.swift
@@ -11,8 +11,7 @@ import UIKit
 //let PPAPIURL = "http://34.253.160.180"
 //let PPAPIURL = "http://35.158.218.151"
 //let PPAPIURL = "http://api.payproapp.net"
-let PPAPIURL = "http://192.168.1.202"
-let PPLocalAPIURL = "http://192.168.1.202"
+let PPAPIURL = "http://192.168.1.201"
 
 
 func makePostRequest(paramsDictionary: NSDictionary, endpointURL: String, completion: @escaping (_ json: NSDictionary) -> Void)
@@ -38,6 +37,10 @@ func makePostRequest(paramsDictionary: NSDictionary, endpointURL: String, comple
             let task = URLSession.shared.dataTask(with: request as URLRequest)
             {
                 (data, response, error) -> Void in
+                
+                print("data: \(data)")
+                print("response: \(response)")
+                print("error: \(error)")
                 
                 if (error != nil)
                 {
