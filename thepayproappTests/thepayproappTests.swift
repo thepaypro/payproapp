abe0012121a15e87d0b2cb7700b1bf71042a94ef
ottegu <>
2018-01-18 14:42:00 +0100

    Add initial test

--- thepayproappTests/thepayproappTests.swift
+++ thepayproappTests/thepayproappTests.swift
@@ -9,11 +9,16 @@
 import XCTest
 @testable import thepayproapp
 
-class thepayproappTests: XCTestCase {
+class PayProAppTests: XCTestCase {
+    var vcLogin: PPPasscodeViewController!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc: PPPasscodeViewController = storyboard.instantiateViewController(withIdentifier: "PPPasscodeViewController") as! PPPasscodeViewController
+        vcLogin = vc
+        _ = vcLogin.view // To call viewDidLoad
     }
     
     override func tearDown() {
@@ -21,16 +26,11 @@ class thepayproappTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUserCreate() {
+        vcLogin.changePassword = false
+        vcLogin.userUsername = "+34691487998"
+        vcLogin.passcodeTF.text = "1234"
+        
+        vcLogin.nextTapped()
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
